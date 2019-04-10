5847d23aa3081acf7d5af1cbac4de3df12a6564d
Nelly <pnpnpnpnpnpn@yandex.ru>
2019-04-10 16:39:48 +0300

    Added SVGs, jquery,axios, changed script

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,41 @@ var app=new Vue({
 	data:{
         show_today:true,
         show_tom:false,
-        show_dat:false
+        show_dat:false,
+        weather_data:''
     },
-    methods: {},
+    mounted:function(){
+        this.getWeather();
+    },
+    methods: {
+        getWeather: function(event) {
+            axios.get('https://gist.githubusercontent.com/anonymous/feb1b31516f3e36a14b29657701f18d2/raw/eaa544aed7e3bdee37c6caa2a515f1d4c38fbd4f/weather.json').then(response => {
+                //this.response = console.log(response);
+                weather_data=response.data;
+                console.log(weather_data);
+                console.log((weather_data.list[0].temp.day-273).toFixed());
+            }).catch(
+                error => {
+                    console.log(error);
+                    alert("Ошибка");
+                }
+            );   
+        },
+        drawWeather: function(event){
+            var now = new Date();
+/*            if(now.getHours()>5 && now.getHours()<12){
+                
+            } */
+        }
+    },
     computed: {}
 });
+app.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">Удалить</button>\
+    </li>\
+  ',
+  props: ['title']
+})
