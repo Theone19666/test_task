1ea7d0549beb788acc48c8c5110706fbfc965948
Nelly <pnpnpnpnpnpn@yandex.ru>
2019-04-15 16:43:56 +0300

    js,css,images changed

--- js/scripts.js
+++ js/scripts.js
@@ -31,43 +31,58 @@ var app = new Vue({
                 //this.response = console.log(response);
                 weather_data=response.data;
                 console.log(weather_data);
-                console.log((weather_data.list[0].temp.day-273).toFixed());
                 weather_list=[
                           {
                             id: 1,
                             text: weather_data.list[0].weather[0].description,
-                            morn: weather_data.list[0].temp.morn,  
-                            day: weather_data.list[0].temp.day,
-                            eve: weather_data.list[0].temp.eve,
-                            night: weather_data.list[0].temp.night,
+                     /*       morn: (weather_data.list[0].temp.morn-273).toFixed(),  
+                            day: (weather_data.list[0].temp.day-273).toFixed(),
+                            eve: (weather_data.list[0].temp.eve-273).toFixed(),
+                            night: (weather_data.list[0].temp.night-273).toFixed(),*/
+                            temperature: this.getTemperature(),
                             show_param_name: "show_today",
                             show: true,
                             show2: false,
-                            show3: false
+                            show3: false,
+                            city: weather_data.city.name,
+                            day: this.getToday().getDate(), 
+                            month: app.getToday().toLocaleString('ru', { month: 'long' }), 
+                            year: this.getToday().getFullYear(),
+                            image: this.getImage(weather_data.list[0].weather[0].description)
                           },
                           {
                             id: 2,
                             text: weather_data.list[1].weather[0].description,
-                            morn: weather_data.list[1].temp.morn,  
-                            day: weather_data.list[1].temp.day,
-                            eve: weather_data.list[1].temp.eve,
-                            night: weather_data.list[1].temp.night,
+                            morn: (weather_data.list[1].temp.morn-273).toFixed(),  
+                            day: (weather_data.list[1].temp.day-273).toFixed(),
+                            eve: (weather_data.list[1].temp.eve-273).toFixed(),
+                            night: (weather_data.list[1].temp.night-273).toFixed(),
                             show_param_name: "show_tom",
                             show: false,
                             show2: true,
-                            show3: false
+                            show3: false,
+                            city: weather_data.city.name,
+                            day: this.getTomorrow().getDate(), 
+                            month: app.getTomorrow().toLocaleString('ru', { month: 'long' }),
+                            year: this.getTomorrow().getFullYear(),
+                            image: this.getImage(weather_data.list[1].weather[0].description)
                           },
                           {
                             id: 3,
                             text: weather_data.list[2].weather[0].description,
-                            morn: weather_data.list[2].temp.morn,  
-                            day: weather_data.list[2].temp.day,
-                            eve: weather_data.list[2].temp.eve,
-                            night: weather_data.list[2].temp.night,
+                            morn: (weather_data.list[2].temp.morn-273).toFixed(),  
+                            day: (weather_data.list[2].temp.day-273).toFixed(),
+                            eve: (weather_data.list[2].temp.eve-273).toFixed(),
+                            night: (weather_data.list[2].temp.night-273).toFixed(),
                             show_param_name: "show_dat",
                             show:false,
                             show2: false,
-                            show3: true
+                            show3: true,
+                            city: weather_data.city.name,
+                            day: this.getDAT().getDate(), 
+                            month: app.getDAT().toLocaleString('ru', { month: 'long' }), 
+                            year: this.getDAT().getFullYear(),
+                            image: this.getImage(weather_data.list[2].weather[0].description)
                           }
                     ]
                     this.weather_list = weather_list;
@@ -78,8 +93,72 @@ var app = new Vue({
                 }
             );   
         },
-        drawWeather: function(event){
+        getNow: function(){
             var now = new Date();
+            var day=now.getDate();
+            var month=now.getMonth();
+            var year=now.getFullYear();
+            var now_json='{"day":'+now.getDate()+',"month":'+month+',"year":'+year+'}';
+            return now_json;
+        },
+        getToday: function(){
+            var get_json=this.getNow();
+            var json=JSON.parse(get_json);
+            var today=new Date(json.year, json.month, json.day);
+            return today; 
+        },    
+        getTomorrow: function(){
+            var get_json=this.getNow();
+            var json=JSON.parse(get_json);
+            var tomorrow=new Date(json.year, json.month, json.day+1);
+            return tomorrow; 
+        },  
+        getDAT: function(){
+            var get_json=this.getNow();
+            var json=JSON.parse(get_json);
+            var dat=new Date(json.year, json.month, json.day+2);
+            return dat; 
+        },
+        getImage: function(text){
+            var now = new Date();
+            var hour=now.getHours()+1;
+            if(hour>21 && hour<6){
+                return text+" night";
+            }
+            else{
+                return text;
+            }
+        },
+        getTemperature: function(){
+            var now = new Date();
+            var hour=now.getHours()+1;
+            if(hour>5 && hour<12){
+                return (weather_data.list[0].temp.morn-273).toFixed();
+            }
+            else if(hour>=12 && hour<18){
+               return (weather_data.list[0].temp.day-273).toFixed();
+            }
+            else if(hour>=18 && hour<=23){
+                return (weather_data.list[0].temp.eve-273).toFixed();
+            }
+            else{
+                return (weather_data.list[0].temp.night-273).toFixed();
+            }
         }
+  /*      getDay: function(event){
+            var now = new Date();
+            return now.getDate();
+        },
+        getMonth: function(event){
+            var now = new Date();
+            var day = now.getDate();
+            return new Date().toLocaleString('ru', {       
+                    month: 'long'       
+            });       
+        },
+        getYear: function(event){
+            var now = new Date();
+            return now.getFullYear();     
+        }, */
     },
 });
