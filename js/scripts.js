f6832f13485ca248f70afa154a62449bde8c5cc8
Nelly <pnpnpnpnpnpn@yandex.ru>
2019-04-16 15:49:35 +0300

    js,html,css

--- js/scripts.js
+++ js/scripts.js
@@ -89,7 +89,7 @@ var app = new Vue({
             }).catch(
                 error => {
                     console.log(error);
-                    alert("Ошибка");
+                    alert("Ошибка соединения с сервером. Пожалуйста, повторите попытку.");
                 }
             );   
         },
